55b6822744a3bde4187a2fddaf416c0b59372a37
Juan Amedh Sanchez Salas <juanamedh.usa@gmail.com>
2021-07-24 09:13:02 -0700

    Coded triangles consts

---
+++ figures.js
@@ -0,0 +1,23 @@
+// Square's code
+const squareSide = 5;
+console.log("The sides of the square are: " + squareSide + "cm");
+
+const squarePerimeter = squareSide * 4;
+console.log("The perimeter of teh square is: " + squarePerimeter + "cm");
+
+const squareArea = squareSide ** 2;
+console.log("The area of the square is: " + squareArea + "cm^2");
+
+// Triangle's code
+const triangleSide1 = 6;
+const triangleSide2 = 6;
+const triangleBase = 4;
+console.log(
+    "The sides of my triangle are: " 
+    + triangleSide1 
+    + "cm, " 
+    + triangleSide2 
+    + "cm, " 
+    + triangleBase 
+    + "cm"
+);
