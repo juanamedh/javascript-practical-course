8b3d1d0f22dd1377306500067d82256c2182a899
Juan Amedh Sanchez Salas <juanamedh.usa@gmail.com>
2021-08-07 11:46:43 -0700

    corrected typo

--- discounts.js
+++ discounts.js
@@ -1,12 +1,19 @@
 const originalPrice = 100;
 const discount = 15;
 
-const discountedPricePercentage = 100 - discount;
-const discountedPrice = (originalPrice * discountedPricePercentage)/100;
+// const discountedPricePercentage = 100 - discount;
+// const discountedPrice = (originalPrice * discountedPricePercentage)/100;
 
-console.log({
-    originalPrice,
-    discount,
-    discountedPricePercentage,
-    discountedPrice,
-});
+function calculateDiscountedPrice(price, discount) {
+    const discountedPricePercentage = 100 - discount;
+    const discountedPrice = (price * discountedPricePercentage)/100;
+    
+    return discountedPrice;
+}
+
+// console.log({
+//     originalPrice,
+//     discount,
+//     discountedPricePercentage,
+//     discountedPrice,
+// });
