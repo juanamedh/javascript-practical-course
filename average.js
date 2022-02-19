d715dca1a9592aee6ea504c7575fc14279f30871
Juan Sanchez <juanamedh.usa@gmail.com>
2022-02-19 15:02:20 -0500

    updated for cicle with the reduce method

--- average.js
+++ average.js
@@ -1,11 +1,17 @@
 
 function computeArithmeticAverage(list) {
-    let additionList = 0;
-    for (let i = 0; i < list.length; i++) {
-    additionList = additionList + list[i];
-    }
+    // let additionList = 0;
+    // for (let i = 0; i < list.length; i++) {
+    // additionList = additionList + list[i];
+    // }
+
+    const additionList = list.reduce(
+        function(accumulatedValue = 0, newElement) {
+            return accumulatedValue + newElement;
+        }
+    );
 
     const averageList = additionList / list.length;
-    
+
     return averageList;
 }
