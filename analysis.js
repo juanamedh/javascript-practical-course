834a241ca5a2d738a6ed10965be84b68d87b0dbf
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-08-12 11:37:36 -0700

    updated metadata

--- analysis.js
+++ analysis.js
@@ -13,3 +13,32 @@ const salariesColSorted = salariesCol.sort(
 function isPair(number) {
     return (number % 2 === 0);
 };
+
+function computeArithmeticAverage(list) {
+    const additionList = list.reduce(
+        function(accumulatedValue = 0, newElement) {
+            return accumulatedValue + newElement;
+        }
+    );
+    const averageList = additionList / list.length;
+    return averageList;
+};
+
+function salariesMedian(list) {
+    const half = parseInt(list.lenght / 2);
+    
+    if (isPair(list.lenght)) {
+        const personHalf1 = list[half - 1];
+        const personHalf2 = list[half];
+
+        const median = computeArithmeticAverage([personHalf1, personHalf2]);
+        return median;
+    } else {
+        const personHalf = list[half];
+        return personHalf;
+    }
+};
+
+console.log(
+    salariesMedian(salariesColSorted)
+    );
